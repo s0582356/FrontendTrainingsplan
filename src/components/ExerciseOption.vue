4fcb82f9399ca7c1e190dd8734d9067787896a57
Your Name <your.email@example.com>
2025-01-20 13:32:29 +0100

    Frontend-Aufrufe zum Backend (Ansehen und Hinzufügen funktioniert; Bearbeiten und Löschen funktioniert nicht)

--- src/components/ExerciseOption.vue
+++ src/components/ExerciseOption.vue
@@ -11,18 +11,52 @@ export default defineComponent({
 
   data() {
     return {
-      übungsListe: [] as Trainingsplan[]
+      übungsListe: [] as Trainingsplan[],
+      newPlan: {
+        uebungNr: 0,
+        uebungName: '',
+        anzahlSaetze: 0,
+        anzahlWiederholungen: 0
+      } as Trainingsplan
     };
   },
-  mounted () {
-
+  mounted() {
     axios
       .get<Trainingsplan[]>(apiEndpoint)
       .then((response) => (this.übungsListe = response.data))
       .catch((error) => console.log(error))
-}
+  },
+  methods: {
+    addNewTrainingPlan() {
+      axios
+        .post<Trainingsplan>(apiEndpoint, this.newPlan)
+        .then((response) => {
+          this.übungsListe.push(response.data)
+          this.newPlan = { uebungNr: 0, uebungName: '', anzahlSaetze: 0, anzahlWiederholungen: 0 }
+        })
+        .catch((error) => console.log(error))
+    },
+    updateTrainingPlan(id: number) {
+      axios
+        .put<Trainingsplan>(`${apiEndpoint}/${id}`, this.newPlan)
+        .then((response) => {
+          const index = this.übungsListe.findIndex(plan => plan.uebungNr === id)
+          if (index !== -1) {
+            this.übungsListe[index] = response.data
+          }
+        })
+        .catch((error) => console.log(error))
+    },
+    deleteTrainingPlan(id: number) {
+      axios
+        .delete(`${apiEndpoint}/${id}`)
+        .then(() => {
+          this.übungsListe = this.übungsListe.filter(plan => plan.uebungNr !== id)
+        })
+        .catch((error) => console.log(error))
+    }
+  }
 })
-
 </script>
 
 <template>
@@ -31,16 +65,21 @@ export default defineComponent({
     <div v-if="übungsListe.length > 0">
       <ul>
         <li v-for="übung in übungsListe" :key="übung.uebungNr">
-          {{ übung }}
+          {{ übung.uebungName }} - {{ übung.anzahlSaetze }} sets x {{ übung.anzahlWiederholungen }} reps
+          <button @click="updateTrainingPlan(übung.uebungNr)">Update</button>
+          <button @click="deleteTrainingPlan(übung.uebungNr)">Delete</button>
         </li>
       </ul>
     </div>
     <div v-else>
       <p>Die Liste hat keinen Eintrag.</p>
     </div>
+    <input v-model="newPlan.uebungName" placeholder="Übung Name" />
+    <input v-model.number="newPlan.anzahlSaetze" placeholder="Anzahl Sätze" type="number" />
+    <input v-model.number="newPlan.anzahlWiederholungen" placeholder="Anzahl Wiederholungen" type="number" />
+    <button @click="addNewTrainingPlan">Add New Training Plan</button>
   </div>
 </template>
 
 <style scoped>
-
 </style>
